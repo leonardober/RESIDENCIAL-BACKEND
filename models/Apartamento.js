9de30ec423ba8630bba427fa13b9b389d9a2ed16
Leonardo <leonardober@gmail.com>
2025-04-17 17:35:20 -0500

    segundo conexion front-back-registro de datosAtlas ResidenciaDb Verdadero

--- models/Apartamento.js
+++ models/Apartamento.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+const residenteSchema = new mongoose.Schema({
+  nombre: String,
+  tipoDocumento: String,
+  documento: String,
+  telefono: String,
+});
+
 const apartamentoSchema = new mongoose.Schema({
   numero: {
     type: String,
@@ -10,12 +17,13 @@ const apartamentoSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  residentes: [{
+  propietario: {
     nombre: String,
     tipoDocumento: String,
     documento: String,
     telefono: String,
-  }],
+  },
+  residentes: [residenteSchema],
 });
 
 const Apartamento = mongoose.model('Apartamento', apartamentoSchema);
